cbf06a9b8134c405c36992ab7de969be79aaac4b
SouhirMahmoudi <85115641+SouhirMahmoudi@users.noreply.github.com>
2022-06-05 22:06:13 +0200

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
