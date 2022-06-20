0d7c2b81a601a3324f4b9e7506e54e36f0218130
SouhirMahmoudi <souhir.mahmoudi2021@campus-eni.fr>
2022-06-20 16:06:22 +0200

    fix:

--- scripts/components/LikesInsert.js
+++ scripts/components/LikesInsert.js
@@ -20,7 +20,8 @@ export default class LikesInsert extends Component {
         this.DOM.innerHTML = `
         <div id="insert" class="insert_description">
         <div class="Likes">
-        <div tabindex="0" id="update" aria-labelledby="nombre de likes total pour les realisations de ${this.name}> </div>
+        <div tabindex="0" id="update" aria-label="nombre de likes total pour les réalisations de ${this.name}">
+       </div>
         <button class="Heart" tabindex="-1"></button>
         </div>
         <div id="price" tabindex="0">${this.price} €/jour </div>
